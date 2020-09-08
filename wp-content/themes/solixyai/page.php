ec51f72138cbdcd0a9d863723134556895181009
Jihed Goui <jihed.gouay@gmail.com>
2020-09-08 14:46:27 +0100

    Blog Archive, Posts, Pagination, Thumbnails

--- wp-content/themes/solixyai/page.php
+++ wp-content/themes/solixyai/page.php
@@ -5,7 +5,11 @@ get_header(); // function automatically know to grab a file header.php
 ?>
 <div class="page-wrap">
     <div class="container">
+
         <h1><?php the_title(); ?></h1>
+        <?php if (has_post_thumbnail()) : ?>
+            <img class="img-fluid img-thumbnail mb-3 mr-3" src="<?php the_post_thumbnail_url('blog-large'); ?>" alt="<?php the_title() ?>">
+        <?php endif; ?>
         <?php get_template_part('includes/section', 'content'); // to separte logic and to keep anything organise 
         ?>
     </div>
